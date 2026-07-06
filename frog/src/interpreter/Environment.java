3c14c6e4bae1eb812d60b99ace193ae58c303813
someonecat92 <someonecat@icloud.com>
2026-07-06 14:57:37 +0300

    Add files via upload

---
+++ frog/src/interpreter/Environment.java
@@ -0,0 +1,16 @@
+package frog.interpreter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Environment {
+    private final Map<String, Object> variables = new HashMap<>();
+
+    public void setVariable(String name, Object value) {
+        variables.put(name, value);
+    }
+
+    public Object getVariable(String name) {
+        return variables.get(name);
+    }
+}
